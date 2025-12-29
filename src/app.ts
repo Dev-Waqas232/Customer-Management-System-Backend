3a493a9b5d0198cfc47cd06b2a80acd044a2e1ae
Dev-Waqas232 <devwaqas232@gmail.com>
2025-12-29 21:44:00 +0500

    feat: added deployment configs for vercel

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express, { Request, Response, type Express } from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import { ApiResponse } from "./utils/api-response";
+import { connectDB } from "./config/db";
 
 config();
+connectDB();
 
 const app: Express = express();
-const port = process.env.PORT ?? 5000;
 
 app.use(cors());
 
@@ -14,4 +15,11 @@ app.use("/health", (req: Request, res: Response) => {
   return ApiResponse.success(res, null, "Hello from Server");
 });
 
-app.listen(port, () => {});
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server is running`);
+  });
+}
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error: ", error);
